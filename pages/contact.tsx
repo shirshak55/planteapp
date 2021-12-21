c8cf64b63dbebf67a60b3f4bc249a9e223a21d26
shirshak55 <shirshak55@gmail.com>
2021-12-21 14:15:21 +0545

    integrate plante api

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,11 @@
 export default function Contact() {
     return (
         <>
-            <p>To be updated</p>
+            <div className="h-full w-full flex justify-center items-center m-8">
+                <div className=" c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden p-8">
+                    Contact Us on plante.application@gmail.com
+                </div>
+            </div>
         </>
     )
 }
